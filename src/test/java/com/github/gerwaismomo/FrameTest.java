e2c187cc4f0813d500eaf08d3a823c226eb1b5c4
gmomo <gerwais.momo@sword-group.com>
2021-03-02 00:16:21 +0100

    Frame with One char except x is not valid (G)

--- src/test/java/com/github/gerwaismomo/FrameTest.java
+++ src/test/java/com/github/gerwaismomo/FrameTest.java
@@ -6,15 +6,25 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(SpringExtension.class)
 public class FrameTest {
 
     @Test
     public void frame_strike_isValid() {
         Roll roll = new Roll('x');
-
         Frame frame = new Frame();
         frame.addRoll(roll);
         assertTrue(frame.isValid());
+    }
+
+    @Test
+    public void frame_withOneCharNotX_isNotValid() {
+        Roll roll = new Roll('1');
+        Frame frame = new Frame();
+        frame.addRoll(roll);
+        assertFalse(frame.isValid());
     }
 }
